56f1f743fab88bfbc8bae43c39481f6d015099a5
clamtacos <5408736+Clamtacos@users.noreply.github.com>
2022-09-14 16:38:05 -0400

    PowerShelll Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,9 @@
   .DESCRIPTION
     Customized personal PowerShell profile
   .LINK
-    
+    https://github.com/Clamtacos/.dotfiles
 #>
 
 # scoop-search
 Invoke-Expression (&scoop-search --hook)
+
