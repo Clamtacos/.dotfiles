69534e01d6644935da4fd33beda977692e7cc741
clamtacos <5408736+Clamtacos@users.noreply.github.com>
2025-11-09 17:53:34 -0500

    Added TITLE field

--- ll.cmd
+++ ll.cmd
@@ -1,9 +1,11 @@
 @ECHO OFF
+REM
 REM	===========================================================================
 REM	 Created on:   	11/08/2025 
 REM	 Created by:   	Doctor Mister Danger
 REM	 Filename:     	ll.cmd
 REM  Description:   An alias for lsd that gives it DIR.EXE type of output.
 REM	===========================================================================
+REM
+TITLE List Location
 lsd --all --color=always --icon=always --icon-theme=unicode --long --human-readable --hyperlink=always --group-dirs=first --literal --system-protected --blocks=name,size,permission
-
