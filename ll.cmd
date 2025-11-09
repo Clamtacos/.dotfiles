72effd63a98a663e0651749e5a45ae8a843f15ec
clamtacos <5408736+Clamtacos@users.noreply.github.com>
2025-11-08 19:49:28 -0500

    Update ll.cmd

--- ll.cmd
+++ ll.cmd
@@ -2,7 +2,7 @@
 REM	===========================================================================
 REM	 Created on:   	11/08/2025 
 REM	 Created by:   	Doctor Mister Danger
-REM	 Filename:     	ll.bin
+REM	 Filename:     	ll.cmd
 REM  Description:   An alias for lsd that gives it DIR.EXE type of output.
 REM	===========================================================================
 lsd --all --color=always --icon=always --icon-theme=unicode --long --human-readable --hyperlink=always --group-dirs=first --literal --system-protected --blocks=name,size,permission
